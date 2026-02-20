72a3892c611eeac626dd35c1fcdcdf2510f2fdec
AlvesKZ <igoralvex01@gmail.com>
2026-02-20 17:06:37 -0300

    feat: implement delete by id endpoint

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -11,6 +11,7 @@ router.get('/', (_, res) => {
 router.get('/cidades', CidadesController.getAllValidation , CidadesController.getAll);
 router.get('/cidades/:id', CidadesController.getByIdValidation , CidadesController.getById);
 router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById);
+router.delete('/cidades/:id', CidadesController.deleteByIdValidation , CidadesController.deleteById);
 router.post('/cidades', CidadesController.createValidation , CidadesController.create);
 
 export { router };
