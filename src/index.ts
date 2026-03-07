9334875a4b948f7d3ae8894d437754da8d867f01
AlvesKZ <igoralvex01@gmail.com>
2026-03-07 16:12:53 -0300

    feat: create user controller

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Knex } from './server/database/knex';
 import { server } from './server/Server';
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3003;
 
 const startServer = () => {
     server.listen(port, () => {
